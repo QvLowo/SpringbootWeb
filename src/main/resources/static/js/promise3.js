23c7b1a002f22a097eceb8da47b3e4e5204c9119
qul <qvlywk@gamil.com>
2024-05-20 11:12:12 +0800

    [同步非同步]遠端非同步查詢，點擊按鈕執行樂透查詢時還可以做別的事情．

--- src/main/resources/static/js/promise3.js
+++ src/main/resources/static/js/promise3.js
@@ -13,9 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
         // 非同步，response完成有時間所以也要加await，等待完成再往下執行
         const {state, message, data} = await response.json();
         console.log(state, message, data);
-
+        // 做其他事情(同步)，等上面做完才會執行這行
         console.log('doSomething~~');
 
         console.log('end');
+    });
+
+    // 非同步
+    $('queryButton2').addEventListener("click",(event)=>{
+        console.log('begin');
+        // 遠端抓資料
+        fetch('http://localhost:8081/data/lotto')
+            // 接到response轉換成json，會花一點時間
+           .then(response => response.json())
+           .then(({state, message, data}) => {
+               console.log(state, message, data);
+               console.log('end');
+           });
+        // 做其他事情（非同步）
+        console.log('doSomething~~');
     });
 });
